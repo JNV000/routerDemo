3f622223065f90e5ba705e89b2060487a304b459
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-30 19:39:39 -0600

    Add basic styles

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -1,8 +1,12 @@
-
 export default function FormButton() {
-    return (
-        <form method="POST">
-            <button type="submit">Submit</button>
-        </form>
-    )
+  return (
+    <form method="POST">
+      <button
+        className="flex items-center rounded bg-slate-100 p-2 text-sky-500"
+        type="submit"
+      >
+        Submit
+      </button>
+    </form>
+  );
 }
