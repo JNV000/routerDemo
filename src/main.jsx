674e62879bdfa42f4b0a1be295ce8b0dbf169e47
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-30 20:15:21 -0600

    Render Contact on Home page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Contact from "./routes/Contact";
+import Error from "./routes/Error";
 import Home from "./routes/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/contacts:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
