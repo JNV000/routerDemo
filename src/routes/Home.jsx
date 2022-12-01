5cea86c927df650c409729953ab9c689d40d0b1c
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-30 19:40:14 -0600

    Add styles to Home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,16 @@
 import Button from "../components/FormButton";
-import Search from "../components/Search";
 import Nav from "../components/Nav";
+import Search from "../components/Search";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen w-1/5 bg-slate-200 pl-10">
       <h1>React Router Contacts</h1>
-      <Search />
-      <Button />
+      <div className="flex items-center border-b border-black">
+        <Search />
+        <Button />
+      </div>
       <Nav />
-    </>
+    </div>
   );
 }
